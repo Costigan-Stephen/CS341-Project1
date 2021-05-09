3103abae230b7f2616163c6522a4f84aa2f38403
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-05-08 22:58:56 -0500

    updated forms

--- routes/prove02-routes.js
+++ routes/prove02-routes.js
@@ -31,7 +31,7 @@ router.post('/addBook', (req, res, next) => {
     var bookId = bookList.length;
     var newBook = { "id": bookId, "title": req.body.title, "year": parseInt(req.body.year), "image": req.body.image, "summary": req.body.summary };
     bookList.books.push(newBook);
-    fs.writeFile('/data/books.json', JSON.stringify(bookList), 'utf8', function(err) {
+    fs.writeFile('./data/books.json', JSON.stringify(bookList), 'utf8', function(err) {
         if (err) throw err;
         console.log('File updated');
     });

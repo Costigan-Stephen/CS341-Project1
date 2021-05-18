ae7d67d13072d0fda3a13c6e147d2e39bbe8285b
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-05-18 00:09:57 -0500

    styling and sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,30 +9,22 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('60a0b694c695c6bdf0f9e924')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                console.log(err);
-                res.redirect('/');
-            });
-        })
-        .catch(err => console.log(err));
+    session.isLoggedIn = true;
+    res.redirect('/');
 };
 
-
 exports.postLogout = (req, res, next) => {
-    req.session.destroy(err => {
-        console.log(err);
-        res.redirect('/');
-    });
+    session.isLoggedIn = false;
+    res.redirect('/');
 };
 
 exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        isAuthenticated: false
+        isAuthenticated: false,
+        isLoggedIn: session.isLoggedIn
     });
 };
+
+exports.postSignup = (req, res, next) => {};

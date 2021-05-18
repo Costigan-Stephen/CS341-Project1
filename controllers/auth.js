92b050aafef0a9bc2f452bf849ca48b1ecd7067d
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-05-17 23:57:32 -0500

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,11 +9,30 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('60a0b694c695c6bdf0f9e924')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
 };
 
+
 exports.postLogout = (req, res, next) => {
-    session.isLoggedIn = false;
-    res.redirect('/');
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,8 @@ router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/logout', authController.postLogout);
 
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
+
+
 module.exports = router;
